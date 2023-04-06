77058867f6d914e566102dbc45967e79d9235fca
Avery <averycaldwell7@gmail.com>
2023-04-06 15:38:04 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// MUI
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+//Components
+import Header from '../src/components/Header';
+import About from '../srComponents/About';
+import Resume from './Components/Resume';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const [currentTab, handleTabChange] = useState('about');
+    // This method is checking to see what the value of `currentTab` is. Depending on the value of currentPage, we return the corresponding component to render.
+    const renderTab = () => {
+        if (currentTab === 'About') {
+            return <About />;
+        }
+        if (currentTab === 'Contact') {
+            return <Contact />;
+        }
+        if (currentTab === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentTab === 'Resume') {
+            return <Resume />;
+        }
+        return <About />;
+    };
+    return (
+        <>
+            ;
+            <Header
+                currentTab={currentTab}
+                handleTabChange={handleTabChange}
+            ></Header>
+            <main>{renderTab()}</main>
+            <Footer></Footer>
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+export default function MyApp() {
+    return (
+        <div>
+            <Button variant='contained'>Hello World</Button>
+        </div>
+    );
+}
+root.render(
+    <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <App />
+    </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
