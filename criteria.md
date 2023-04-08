5fbb61b41bfc40b5b66140d4369cb20b8497e7e3
Avery <averycaldwell7@gmail.com>
2023-04-08 14:11:53 -0700

    updated portfolio component

--- criteria.md
+++ criteria.md
@@ -21,31 +21,40 @@ SO THAT I can assess whether they're a good candidate for an open position
 ## Acceptance Criteria
 
 ```md
-GIVEN a single-page application portfolio for a web developer
-WHEN I load the portfolio
-THEN I am presented with a page containing a header, a section for content, and a footer
-WHEN I view the header
-THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio
-WHEN I view the navigation titles
-THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
+<!-- GIVEN a single-page application portfolio for a web developer -->
+<!-- WHEN I load the portfolio -->
+<!-- THEN I am presented with a page containing a header, a section for content, and a footer -->
+<!-- WHEN I view the header -->
+<!-- THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio -->
+<!-- WHEN I view the navigation titles -->
+TODO: 
+THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is **HIGHLIGHTED**
+TODO: 
 WHEN I click on a navigation title
-THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
+THEN I am presented with the corresponding section below the navigation without the page reloading and that title is **HIGHLIGHTED**
+TODO: 
 WHEN I load the portfolio the first time
-THEN the About Me title and section are selected by default
+THEN the About Me title and section are selected by default**
+TODO: 
 WHEN I am presented with the About Me section
 THEN I see a recent photo or avatar of the developer and a short bio about them
+TODO: 
 WHEN I am presented with the Portfolio section
-THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repositories
+THEN I see titled images of six of the developer’s applications with links to both the deployed 
+applications and the corresponding GitHub repositories
+TODO: 
 WHEN I am presented with the Contact section
 THEN I see a contact form with fields for a name, an email address, and a message
+TODO: 
 WHEN I move my cursor out of one of the form fields without entering text
 THEN I receive a notification that this field is required
 WHEN I enter text into the email address field
 THEN I receive a notification if I have entered an invalid email address
+TODO: 
 WHEN I am presented with the Resume section
 THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
-WHEN I view the footer
-THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter)
+<!-- WHEN I view the footer -->
+<!-- THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter) -->
 ```
 
 ## Mock-Up
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as Material from '@mui/material';
 
 function Portfolio({ currentPage }) {
     return (
@@ -7,14 +8,104 @@ function Portfolio({ currentPage }) {
                 <div
                     style={{
                         height: 'calc(100vh - 114px)',
+                        backgroundPosition: 'center',
+                        backgroundColor: '#c9e4ca',
                     }}
                 >
                     {/* everything goes inside this div */}
-                    <p>Portfolio TestTest</p>
+                    <Material.Box
+                        sx={{
+                            color: '#364958',
+                            fontSize: '3rem',
+                            display: 'flex',
+                            justifyContent: 'space-evenly',
+                            alignItems: 'center',
+                            mx: 'auto',
+                            p: 5,
+                            overflowY: 'none',
+                        }}
+                    >
+                        My Portfolio
+                    </Material.Box>
+                    <Material.ImageList sx={{ width: 500, height: 450 }}>
+                        <Material.ImageListItem key='Subheader' cols={2}>
+                            <Material.ListSubheader component='div'>
+                                Projects
+                            </Material.ListSubheader>
+                        </Material.ImageListItem>
+                        {itemData.map((item) => (
+                            <Material.ImageListItem key={item.img}>
+                                <img
+                                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                    alt={item.title}
+                                    loading='lazy'
+                                />
+                                <Material.ImageListItemBar
+                                    title={item.title}
+                                    subtitle={item.author}
+                                    actionIcon={
+                                        <Material.IconButton
+                                            sx={{
+                                                color: 'rgba(255, 255, 255, 0.54)',
+                                            }}
+                                            aria-label={`info about ${item.title}`}
+                                        ></Material.IconButton>
+                                    }
+                                />
+                            </Material.ImageListItem>
+                        ))}
+                    </Material.ImageList>
                 </div>
             )}
         </React.Fragment>
     );
 }
-
+// portfolio images
+const itemData = [
+    {
+        img: 'WYW.png',
+        link: 'https://averycaldwell.github.io/WearYouWander/',
+        repo: 'https://github.com/AveryCaldwell/Team-Profile-Generator',
+        title: 'Wear You Wander',
+        rows: 2,
+        cols: 2,
+        featured: true,
+    },
+    {
+        img: 'CC.png',
+        title: 'Cheers Check',
+        link: 'https://dry-gorge-23555.herokuapp.com/',
+        repo: 'https://github.com/AveryCaldwell/CheersCheck',
+        featured: true,
+    },
+    {
+        img: 'techblog.png',
+        link: ' https://averycaldwell-tech-blog.herokuapp.com/',
+        repo: 'https://github.com/AveryCaldwell/Tech-Blog-MVC',
+        title: 'Tech Blog MVC',
+    },
+    {
+        img: 'employee.png',
+        title: 'Employee Tracker',
+        link: 'https://watch.screencastify.com/v/VsSvyhIgvLkT3GcRarLN',
+        repo: 'https://github.com/AveryCaldwell/Employee-Tracker',
+        cols: 2,
+    },
+    {
+        img: 'team.png',
+        title: 'Team Profile Generator',
+        link: 'https://drive.google.com/file/d/1Y1BxdDhGIa6eY4UsD9mmbSgIKfIHJIZq/view',
+        repo: 'https://github.com/AveryCaldwell/Team-Profile-Generator',
+        cols: 2,
+    },
+    {
+        img: 'weather.png',
+        title: ' Weather Dashboard API',
+        link: 'https://averycaldwell.github.io/weather-dashboard-API/',
+        repo: 'https://github.com/AveryCaldwell/weather-dashboard-API',
+        rows: 2,
+        cols: 2,
+    },
+];
 export default Portfolio;
