096fa0d24b738abb0afee0a0bb91eb77a6a92153
Avery <averycaldwell7@gmail.com>
2023-04-08 15:17:56 -0700

    about component added

--- criteria.md
+++ criteria.md
@@ -32,12 +32,11 @@ THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, an
 TODO: 
 WHEN I click on a navigation title
 THEN I am presented with the corresponding section below the navigation without the page reloading and that title is **HIGHLIGHTED**
+<!-- WHEN I load the portfolio the first time -->
+<!-- THEN the About Me title and section are selected by default** -->
 TODO: 
-WHEN I load the portfolio the first time
-THEN the About Me title and section are selected by default**
-TODO: 
-WHEN I am presented with the About Me section
-THEN I see a recent photo or avatar of the developer and a short bio about them
+<!-- WHEN I am presented with the About Me section -->
+THEN I see a recent **photo or avatar** of the developer and a short bio about them
 TODO: 
 WHEN I am presented with the Portfolio section
 THEN I see titled images of six of the developer’s applications with links to both the deployed 
--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 // import myGif from '../img/computer-bg.gif';
 import * as Material from '@mui/material';
+import * as Image from 'mui-image';
 
 function About({ currentPage }) {
     return (
@@ -22,8 +23,7 @@ function About({ currentPage }) {
                         {/* FIX ME : correct the lin */}
                         <Material.Box
                             sx={{
-                                display: 'flex',
-                                justifyContent: 'space-evenly',
+                                display: 'block',
                                 alignItems: 'center',
                                 '& > *': {
                                     mx: 'auto',
@@ -50,14 +50,67 @@ function About({ currentPage }) {
                                 },
                             }}
                         >
-                            <p>Hi! I'm Avery. Welcome to my Portfolio! </p>
-                            <Material.Stack direction='row' spacing={2}>
-                                <Material.Avatar
-                                    alt='Avery Caldwell'
-                                    src='/img/aves2.png'
+                            <br />
+                            <Material.Box>
+                                <Image.Image
+                                    src='aves2.png'
+                                    style={{
+                                        borderRadius: 16,
+                                        margin: '2rem',
+                                        padding: '2rem',
+                                    }}
                                 />
-                            </Material.Stack>
-                            <p>Explore to learn more! Plz Hire Me</p>
+                            </Material.Box>
+                            <Material.Box
+                                sx={{
+                                    margin: '2rem',
+                                    padding: '2rem',
+                                    borderRadius: 16,
+                                    textAlign: 'center',
+                                    fontSize: '1.30rem',
+                                    fontWeight: 'lighter',
+                                    marginLeft: 'auto',
+                                    marginRight: 'auto',
+                                    paddingLeft: 'auto',
+                                    paddingRight: 'auto',
+                                    width: '75%',
+                                    boxShadow: 2,
+                                    backgroundColor: '#e9f4e9',
+                                }}
+                            >
+                                Hey there! I'm Avery, a full stack developer
+                                with a year of experience in developing web
+                                applications. I'm passionate about building
+                                scalable, responsive, and user-friendly web apps
+                                using a variety of frameworks like Material,
+                                Foundation Framework, Bootstrap, and Anime.js.
+                                Nothing gets me more excited than tackling
+                                complex problems and discovering new ways to
+                                enhance my skills and knowledge.
+                                <br />
+                                <br />
+                                I'm proficient in both front-end and back-end
+                                development, using HTML, CSS, JavaScript, and
+                                jQuery for the former and Node.js, mySQL,
+                                Express.js, and MongoDB for the latter. But,
+                                what sets me apart is my excellent communication
+                                and collaboration skills. I'm a team player, and
+                                I thrive on working alongside other developers,
+                                designers, and stakeholders to deliver
+                                top-quality web applications that meet business
+                                needs.
+                                <br />
+                                <br />
+                                In my spare time, you'll usually find me
+                                tinkering with some side projects, live
+                                streaming, or eating too much pizza. I love
+                                experimenting with new technologies and
+                                frameworks to see how I can use them to create
+                                something innovative and exciting. And if you're
+                                searching for a developer who can bring a dash
+                                of creativity and humor to your team, look no
+                                further - I'm your girl!
+                            </Material.Box>
                             {/* <Material.Button variant='contained'>
                                 Contained
                             </Material.Button> */}
